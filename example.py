0f4ec1ae286ce3330166382d82d0d6924ee1fdbc
yoelbassin <bassi@Yoel-PC.localdomain>
2022-05-30 00:32:27 +0300

    update example

--- example.py
+++ example.py
@@ -2,20 +2,35 @@
 from connectivity import Sector, Connectivity
 import netCDF4 as nc
 
-domain = [-60, -67, -62, -6]
-
-sectors = Sector.create_sectors(domain, 0.1)
-
+# %%
+# get trajectories data
 ncfiles = r"/mnt/c/users/bassi/krill_transport_wap_palmer_canyon_shelf_F2_20200101_30_leeway_50.nc"
-
 ds = nc.Dataset(ncfiles)
-
 lons = ds['lon'][:]
 lats = ds['lat'][:]
-# %%
-c = Connectivity(lons, lats, sectors)
 
 # %%
-c.plot(c.n_settle_values, squares=True, delta=0.1, corners=domain, title='Nsettle')
+## Example for creating from shapefile ###
+# create sectors from shapefile
+shapefile = r"/mnt/c/users/bassi/Downloads/wap_chlora_202001.shp"
+shapefile_sectors = Sector.sectors_from_shapefile(shapefile)
+
 # %%
-c.plot(c.n_supply_values, squares=True, delta=0.1, corners=domain)
+shapefile_model = Connectivity(lons, lats, shapefile_sectors)
+
+# %%
+shapefile_model.plot(shapefile_model.n_settle_values, title="Nsettle")
+
+# %%
+### Example for creating from reactangular domain ###
+# create sectors from domain
+domain = [-60, -67, -62, -6]
+domain_sectors = Sector.create_sectors(domain, 0.1)
+
+# %%
+domain_model = Connectivity(lons, lats, domain_sectors)
+# %%
+domain_model.plot(domain_model.n_settle_values, squares=True, delta=0.1, corners=domain, title='Nsettle')
+# %%
+domain_model.plot(domain_model.n_supply_values, squares=True, delta=0.1, corners=domain)
+
